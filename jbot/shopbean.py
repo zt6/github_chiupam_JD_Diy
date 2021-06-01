1478931fdcfe62803229bf646f9916540d55ad0c
Chiupam <65508083+chiupam@users.noreply.github.com>
2021-06-02 01:23:32 +0800

    1. 修改 getBwan() 函数;2. 检测到 cookie 失效信息时自动屏蔽此账号并发送通知

--- jbot/shopbean.py
+++ jbot/shopbean.py
@@ -172,21 +172,19 @@ async def shopbean(event):
 
 
 # 监控龙王庙频道
-@client.on(events.NewMessage(chats=-1001159808620))
+@client.on(events.NewMessage(chats=-1001159808620, pattern=r'RRA.*'))
 async def redrain(event):
     """
     监控龙王庙
     :param event:
     :return:
     """
-    message = event.message.text # 获取频道发布的消息
-    if 'RRA' in message and '开始' in message and '结束' in message: # 消息中包含 RRA 开始 结束 三个词
-        RRA = re.findall(r"RRA.*", message) # 截取 RRA 字符串
-        input_RRA = '&'.join(RRA) # 处理 RRA 字符串
-        start_time = re.findall(re.compile(r"开.*"), message) # 截取开始时间
-        file = '-'.join(start_time[0].split(' ')[1].split(':')[:-1]) # 定义文件名
-        with open(f'{_LogDir}/{file}.txt', 'w', encoding='utf-8') as f: # 打开 log 下一个新的写入文件
-            print(input_RRA, file=f) # 把 RRA 字符串写入文件中
+    RRA = re.findall(r"RRA.*", event.message.tex) # 截取 RRA 字符串
+    input_RRA = '&'.join(RRA) # 处理 RRA 字符串
+    start_time = re.findall(re.compile(r"开.*"), event.message.tex) # 截取开始时间
+    file = '-'.join(start_time[0].split(' ')[1].split(':')[:-1]) # 定义文件名
+    with open(f'{_LogDir}/{file}.txt', 'w', encoding='utf-8') as f: # 打开 log 下一个新的写入文件
+        print(input_RRA, file=f) # 把 RRA 字符串写入文件中
 
 
 # 监测到用户在任意窗口发送 /checkcookie 指令，则自动临时屏蔽某个过期的cookie
