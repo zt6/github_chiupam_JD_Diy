09f02dbf75aefdf723d1d3e4a62c14eb12793500
chiupam <chiupam@126.com>
2021-06-13 23:00:14 +0800

    修复主队瓜分ID的BUG，使用新的session文件

--- jbot/user.py
+++ jbot/user.py
@@ -3,20 +3,20 @@
 # @Author   : Chiupam (https://t.me/chiupam)
 # @Data     : 2021-06-12 12:14
 # @Version  : v 2.4
-# @Updata   : 1. 修复监控组队瓜分ID的bug；2. 修改监控组队瓜分ID的正则表达式
+# @Updata   : 1.
 # @Future   : 1.
 
 
 from .. import chat_id, jdbot, _ConfigDir, logger, api_id, api_hash, proxystart, proxy, _ScriptsDir, _JdbotDir
-from ..bot.utils import cmd, press_event, backfile, jdcmd, _DiyDir, V4, QL, _ConfigFile
+from ..bot.utils import cmd, press_event, backfile, jdcmd, _DiyDir, V4, QL, _ConfigFile, myck
 from telethon import events, TelegramClient
 import re, json, requests, os, asyncio
 
-
+f
 if proxystart:
-    client = TelegramClient("diy", api_id, api_hash, proxy=proxy, connection_retries=None).start()
+    client = TelegramClient("user", api_id, api_hash, proxy=proxy, connection_retries=None).start()
 else:
-    client = TelegramClient("diy", api_id, api_hash, connection_retries=None).start()
+    client = TelegramClient("user", api_id, api_hash, connection_retries=None).start()
 
 
 with open(f'{_ConfigDir}/bot.json', 'r', encoding='utf-8') as botf:
@@ -29,31 +29,9 @@ if not os.path.isfile('/jd/jbot/diy/bot.py'):
         os.system('pm2 restart jbot')
 
 
-# 从 config.sh 中读取 cookies
-def readCookies():
-    """
-    读取 cookie
-    :return: 最新的 cookies 列表
-    """
-    ckreg = re.compile(r'pt_key=\S*;pt_pin=\S*;')
-    with open(_ConfigFile, 'r', encoding='utf-8') as f:
-        lines = f.read()
-    cookies = ckreg.findall(lines)
-    for cookie in cookies:
-        if cookie == 'pt_key=xxxxxxxxxx;pt_pin=xxxx;':
-            cookies.remove(cookie)
-            break
-    return cookies
-
-
-# 检查 cookie 是否过期的第一个函数
 def checkCookie1():
-    """
-    检测 Cookie 是否过期
-    :return: 返回过期的 Cookie 的账号数字列表
-    """
     expired = []
-    cookies = readCookies()
+    cookies = myck()
     for cookie in cookies:
         cknum = cookies.index(cookie) + 1
         if checkCookie2(cookie):
@@ -61,13 +39,7 @@ def checkCookie1():
     return expired, cookies
 
 
-# 检查 cookie 是否过期的第二个函数
 def checkCookie2(cookie):
-    """
-    检测 Cookie 是否过期
-    :param cookiex: 传入 Cookie
-    :return: 返回是否过期
-    """
     url = "https://me-api.jd.com/user_new/info/GetJDUserInfoUnion"
     headers = {
         "Host": "me-api.jd.com",
@@ -93,38 +65,7 @@ def checkCookie2(cookie):
         return False
 
 
-def checkCrontab(cron, PL, fname, fpath):
-    """
-    新旧命令对比，有新命令则写入新命令
-    """
-    crontab_list = f'{_ConfigDir}/crontab.list'
-    key = f'# {fname}（请勿删除此行）\n'
-    new = f'{cron} {PL} {fpath}\n'
-    with open(crontab_list, 'r', encoding='utf-8') as f1:
-        crontab = f1.readlines()
-    if crontab[-1] == '\n':
-        del (crontab[-1])
-    if key in crontab:
-        m = crontab.index(key) + 1
-        if crontab[m] != new:
-            crontab[m] = new
-            with open(crontab_list, 'w', encoding='utf-8') as f2:
-                f2.write(''.join(crontab))
-    else:
-        crontab.append(f'\n{key}{new}')
-        with open(crontab_list, 'w', encoding='utf-8') as f2:
-            f2.write(''.join(crontab))
-
-
-# 监控布道场频道，检测到关键事件的触发时执行的函数
 def getbean(i, cookie, url):
-    """
-    发起 GET 请求
-    :param i: 账号
-    :param cookie: 传入Cookie
-    :param url: 传入 GET 所需的 url
-    :return: 返回推送的消息主体
-    """
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
         "Accept-Encoding": "gzip,compress,br,deflate",
@@ -206,12 +147,13 @@ async def myexport(event):
                     if config.find("第五区域") != -1 and config.find("↑") != -1:
                         end_line = configs.index(config)
                         break
-                configs.insert(end_line - 2, f'export {kname}="{vname}"{note}\n')
+                configs.insert(end_line - 2, f'export {kname}="{vname}"\n')
                 configs = ''.join(configs)
             else:
                 with open(f"{_ConfigDir}/config.sh", 'r', encoding='utf-8') as f4:
                     configs = f4.read()
-                configs += f'export {kname}="{vname}"{note}\n'
+                configs += f'export {kname}="{vname}"\n'
+            end = "新增环境变量成功"
         with open(f"{_ConfigDir}/config.sh", 'w', encoding='utf-8') as f2:
             f2.write(configs)
         await asyncio.sleep(1.5)
