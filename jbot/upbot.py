7299ba9aa9b71ee5ae272f1e0c8384288c9d0c53
chiupam <chiupam@126.com>
2021-06-18 19:14:26 +0800

    使用 bot 里面的 restart 方法进行重启机器人

--- jbot/upbot.py
+++ jbot/upbot.py
@@ -9,6 +9,7 @@
 
 from .. import chat_id, jdbot, logger, _JdbotDir
 from ..bot.utils import press_event, V4, QL, split_list, row, backfile, mybot
+from ..diy.bot import upuser, restart
 from telethon import events, Button
 from asyncio import exceptions
 import requests, re, os, asyncio
@@ -44,7 +45,6 @@ async def myupbot(event):
                 conv.cancel()
                 return
             elif fname == 'user.py':
-                from ..diy.user import upuser
                 await upuser(fname, msg)
             conv.cancel()
         msg = await jdbot.edit_message(msg, "开始下载文件")
@@ -68,16 +68,3 @@ async def myupbot(event):
         logger.error('something wrong,I\'m sorry\n' + str(e))
 
 
-async def restart():
-    try:
-        if V4:
-            await jdbot.send_message(chat_id, "v4用户，准备重启机器人")
-            os.system("pm2 restart jbot")
-        elif QL:
-            await jdbot.send_message(chat_id, "青龙用户，准备重启机器人")
-            os.system("ql bot")
-        else:
-            await jdbot.send_message(chat_id, "未知用户，自行重启机器人")
-    except Exception as e:
-        await jdbot.send_message(chat_id, 'something wrong,I\'m sorry\n' + str(e))
-        logger.error('something wrong,I\'m sorry\n' + str(e))
