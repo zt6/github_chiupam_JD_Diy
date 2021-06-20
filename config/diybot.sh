966355fdccd6a3ddb3657cbf862d9ffa6c601a26
chiupam <chiupam@126.com>
2021-06-20 23:54:09 +0800

    整合原机器人的升级sh

--- config/diybot.sh
+++ config/diybot.sh
@@ -2,9 +2,9 @@
 # 从 whyour 大佬的 bot.sh 与 E大 的 jup.sh 与 SuMaiKaDe 大佬的 bot.sh 拼凑出来
 ## 导入通用变量与函数
 if [ ! -d "/ql" ];then
-  dir_root=/jd
+    dir_root=/jd
 else
-  dir_root=/ql
+    dir_root=/ql
 fi
 
 dir_bot=$dir_root/jbot
@@ -20,27 +20,27 @@ set_2="${dir_root}/config/diybotset.json"
 user_file="${dir_bot}/diy/user.py"
 
 git_pull_scripts() {
-  local dir_current=$(pwd)
-  local dir_work="$1"
-  local branch="$2"
-  [[ $branch ]] && local cmd="origin/${branch}"
-  cd $dir_work
-  echo -e "开始更新仓库：$dir_work\n"
-  git fetch --all
-  exit_status=$?
-  git reset --hard $cmd
-  git pull
-  cd $dir_current
+    local dir_current=$(pwd)
+    local dir_work="$1"
+    local branch="$2"
+    [[ $branch ]] && local cmd="origin/${branch}"
+    cd $dir_work
+    echo -e "开始更新仓库：$dir_work\n"
+    git fetch --all
+    exit_status=$?
+    git reset --hard $cmd
+    git pull
+    cd $dir_current
 }
 
 git_clone_scripts() {
-  local url=$1
-  local dir=$2
-  local branch=$3
-  [[ $branch ]] && local cmd="-b $branch "
-  echo -e "开始克隆仓库 $url 到 $dir\n"
-  git clone $cmd $url $dir
-  exit_status=$?
+    local url=$1
+    local dir=$2
+    local branch=$3
+    [[ $branch ]] && local cmd="-b $branch "
+    echo -e "开始克隆仓库 $url 到 $dir\n"
+    git clone $cmd $url $dir
+    exit_status=$?
 }
 
 echo -e "\n1、安装bot依赖...\n"
@@ -49,32 +49,32 @@ echo -e "\nbot依赖安装成功...\n"
 
 echo -e "\n2、下载bot所需文件...\n"
 if [ -d ${repo_1}/.git ]; then
-  git_pull_scripts ${repo_1} "main"
+    git_pull_scripts ${repo_1} "main"
 else
-  git_clone_scripts ${url_1} ${repo_1} "main"
+    git_clone_scripts ${url_1} ${repo_1} "main"
 fi
 cp -rf "$repo_1/jbot" $dir_root
 cd $dir_config
 if [[ ! -f "$set_1" ]]; then
-  cp -f "$set_1" $dir_config
+    cp -f "$set_1" $dir_config
 fi
 
 echo -e "\n3、下载diybot仓库文件...\n"
 if [ -d ${repo_2}/.git ]; then
-  git_pull_scripts ${repo_2} "master"
+    git_pull_scripts ${repo_2} "master"
 else
-  git_clone_scripts ${url_2} ${repo_2} "master"
+    git_clone_scripts ${url_2} ${repo_2} "master"
 fi
 cd $dir_bot/diy
 if [ ! -f "$user_file" ]; then
-  cp -rf $repo_2/jbot/. $dir_diy
-  rm -rf $dir_diy/user.py
+    cp -rf $repo_2/jbot/. $dir_diy
+    rm -rf $dir_diy/user.py
 else
-  cp -rf $repo_2/jbot/. $dir_diy
+    cp -rf $repo_2/jbot/. $dir_diy
 fi
 cd $dir_config
 if [ ! -f "$set_2" ]; then
-  cp -rf $set_2 $dir_config
+    cp -rf $set_2 $dir_config
 fi
 
 cd $dir_root
@@ -85,14 +85,13 @@ if [[ -z $(grep -E "123456789" $dir_root/config/bot.json) ]]; then
     if [ -d "/ql" ]; then
         ps -ef | grep "python3 -m jbot" | grep -v grep | awk '{print $1}' | xargs kill -9 2>/dev/null
         nohup python3 -m jbot >$dir_root/log/bot/bot.log 2>&1 &
-        echo -e "bot启动成功...\n"
     else
         cd $dir_bot
         pm2 start ecosystem.config.js
         cd $dir_root
         pm2 restart jbot
-        echo -e "bot启动成功...\n"
     fi
+    echo -e "bot启动成功...\n"
 else
     echo -e  "似乎 $dir_root/config/bot.json 还未修改为你自己的信息，可能是首次部署容器，因此不启动Telegram Bot...\n配置好bot.json后再次运行本程序即可启动"
 fi
