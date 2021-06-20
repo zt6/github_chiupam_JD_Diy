f78e8719e68004e05dd38d7c1954632da166f05b
chiupam <chiupam@126.com>
2021-06-21 00:21:51 +0800

    把 bot.py 的 /restart 指令拆解为单个文件

---
+++ jbot/restart.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author   : Chiupam
+# @Data     : 2021-06-20
+# @Version  : v 1.0
+# @Updata   :
+# @Future   :
+
+
+from .. import chat_id, jdbot, logger
+from ..diy.utils import restart
+from telethon import events
+
+
+@jdbot.on(events.NewMessage(from_users=chat_id, pattern=r'^/restart$'))
+async def myrestart(event):
+    try:
+        await restart()
+    except Exception as e:
+        await jdbot.send_message(chat_id, 'something wrong,I\'m sorry\n' + str(e))
+        logger.error('something wrong,I\'m sorry\n' + str(e))
