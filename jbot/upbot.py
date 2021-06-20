bcfb088edb4b89dcfee147a4ba899fb642872fd1
chiupam <chiupam@126.com>
2021-06-21 01:24:11 +0800

    把 utils.py 的 restart 方法移进此处

--- jbot/upbot.py
+++ jbot/upbot.py
@@ -66,7 +66,6 @@ async def myupbot(event):
         backfile(path)
         with open(path, 'w+', encoding='utf-8') as f:
             f.write(resp)
-        from ..diy.bot import restart
         await restart()
     except exceptions.TimeoutError:
         msg = await jdbot.edit_message(msg, '选择已超时，对话已停止，感谢你的使用')
@@ -74,4 +73,19 @@ async def myupbot(event):
         await jdbot.send_message(chat_id, 'something wrong,I\'m sorry\n' + str(e))
         logger.error('something wrong,I\'m sorry\n' + str(e))
 
+
+# 重启函数
+async def restart():
+    try:
+        if V4:
+            await jdbot.send_message(chat_id, "重启程序")
+            os.system("pm2 restart jbot")
+        elif QL:
+            await jdbot.send_message(chat_id, "重启程序")
+            os.system("ql bot")
+        else:
+            await jdbot.send_message(chat_id, "未知用户，自行重启机器人")
+    except Exception as e:
+        await jdbot.send_message(chat_id, 'something wrong,I\'m sorry\n' + str(e))
+        logger.error('something wrong,I\'m sorry\n' + str(e))
 
