5741de9a83ddb798c746e707296f45180bb8165c
chiupam <chiupam@126.com>
2021-07-15 17:54:11 +0800

    优化

--- beta/aff.py
+++ beta/aff.py
@@ -8,7 +8,9 @@ async def myaff(event):
     try:
         img_file = f"{_JdbotDir}/diy/aff.jpg"
         msg = await jdbot.send_message(chat_id, '感谢您的赞助', file=img_file)
-        await asyncio.sleep(60)
+        for i in range(60):
+            msg = await jdbot.edit_message(msg, f'感谢您的赞助，消息自毁倒计时 {60 - i} 秒')
+            await asyncio.sleep(1)
         await jdbot.delete_messages(chat_id, msg)
     except Exception as e:
         await jdbot.send_message(chat_id, 'something wrong,I\'m sorry\n' + str(e))
