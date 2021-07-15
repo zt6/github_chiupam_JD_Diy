2d3a4067237503f50aa2c7dc7b56e026b9b4eea0
chiupam <chiupam@126.com>
2021-07-15 17:12:01 +0800

    新增 /aff 指令和 aff.png，用于赞助（灰度测试）

--- beta/aff.py
+++ beta/aff.py
@@ -6,7 +6,8 @@ import asyncio
 @jdbot.on(events.NewMessage(from_users=chat_id, pattern=r'^/aff$'))
 async def myaff(event):
     try:
-        msg = await jdbot.send_file(chat_id, "感谢您的赞助", file=f'{_JdbotDir}/diy/aff.png')
+        img_file = f"{_JdbotDir}/diy/aff.jpg"
+        msg = await jdbot.send_message(chat_id, '感谢您的赞助', file=img_file)
         await asyncio.sleep(60)
         await jdbot.delete_messages(chat_id, msg)
     except Exception as e:
