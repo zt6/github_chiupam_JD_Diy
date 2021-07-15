a8bfc1e0af87359c95d45b019592d5783557fdb6
chiupam <chiupam@126.com>
2021-07-15 14:35:23 +0800

    更具不同的用户发送不同的消息

--- jbot/help.py
+++ jbot/help.py
@@ -8,6 +8,7 @@
 
 
 from .. import chat_id, jdbot, logger, TOKEN
+from ..bot.utils import V4, QL
 from telethon import events
 
 
@@ -18,12 +19,39 @@ bot_id = int(TOKEN.split(':')[0])
 async def myhelp(event):
     try:
         msg_id = event.id
-        msg = '''
-a-自定义快捷按钮
-addenv-青龙新增环境变量
+        if V4:
+            msg = '''a-自定义快捷按钮
 bean-获取收支
 chart-统计收支变化
 checkcookie-检测过期
+clearboard-删除快捷输入按钮
+cmd-执行cmd命令
+dl-下载文件
+edit-编辑文件
+export-管理环境变量
+help-获取帮助
+getcookie-扫码获取cookie
+getfile-获取jd目录下文件
+install-扩展此程序功能
+log-选择日志
+list-列出已拓展功能
+node-执行js脚本文件，绝对路径
+restart-重启本程序
+repo-仓库管理
+set-BOT设置
+setname-设置命令别名
+setshort-设置自定义按钮
+snode-选择脚本后台运行
+start-开始使用本程序
+uninstall-删除拓展功能
+up-升级原机器人
+upbot-更新拓展功能
+ver-版本'''
+        elif QL:
+            msg = '''a-自定义快捷按钮
+addenv-青龙新增环境变量
+bean-获取收支
+chart-统计收支变化
 clearboard-删除快捷输入按钮
 cmd-执行cmd命令
 dl-下载文件
@@ -47,8 +75,7 @@ start-开始使用本程序
 uninstall-删除拓展功能
 up-升级原机器人
 upbot-更新拓展功能
-ver-版本
-'''
+ver-版本'''
         await jdbot.edit_message(bot_id, msg_id + 1, msg)
     except Exception as e:
         await jdbot.send_message(chat_id, 'something wrong,I\'m sorry\n' + str(e))
