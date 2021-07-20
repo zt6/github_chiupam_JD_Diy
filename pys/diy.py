18955e69fa8042ab227ad4e840a18401f3625180
“chiupam” <chiupam@126.com>
2021-07-20 23:34:37 +0800

    方便了解如何使用diy.py

--- pys/diy.py
+++ pys/diy.py
@@ -1,11 +1,62 @@
-from .. import _ScriptsDir, _OwnDir
-from ..bot.utils import V4, QL
+"""
+要求：
+    1、需要具备一定的 python3 知识；
+    2、清楚自己需要编辑代码的区域；
+    3、清楚自己需要导入的变量的位置和变量名。
+"""
 
 
-import os
+from .. import chat_id, jdbot, logger, api_id, api_hash, proxystart, proxy, _ConfigDir, _ScriptsDir, _OwnDir, _JdbotDir, _DiyScripts, _LogDir, _shortcut, _botlog, _botjson, img_file, _botset, _set, chname, TOKEN, StartCMD, proxyType, connectionType
+from ..bot.utils import row, _CronFile, bean_log, _ConfigFile, V4, QL, _DiyDir, jdcmd, myck, split_list, backfile, press_event, cmd, getname, logbtn, logbtn, mycron, mycron, upcron, upcron, upcron, upcron, cronmanger, cronmanger
+from telethon import events, Button
+from asyncio import exceptions
 
 
-def diy_1():
-    """
-    自由发挥即可
-    """
+"""
+如果需要导入 diy 目录内某个文件的变量或函数
+from ..diy.xxx import xxx
+例如：from ..diy.utils import bot_id
+"""
+
+
+"""
+如果需要导入其他第三方库
+import xxx
+例如：import json
+"""
+
+
+def smiek_jd_zdjr():
+    try:
+        """
+        try 部分则自由发挥即可
+        """
+
+
+
+
+
+
+
+
+    except Exception as e:
+        await jdbot.send_message(chat_id, 'something wrong,I\'m sorry\n' + str(e))
+        logger.error('something wrong,I\'m sorry\n' + str(e))
+
+
+def jd_joinTeam_activityId():
+    try:
+        """
+        try 部分则自由发挥即可
+        """
+
+
+
+
+
+
+
+
+    except Exception as e:
+        await jdbot.send_message(chat_id, 'something wrong,I\'m sorry\n' + str(e))
+        logger.error('something wrong,I\'m sorry\n' + str(e))
