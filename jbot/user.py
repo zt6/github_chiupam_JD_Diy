cd9e37de8d1bd3f9e10cabcc963d004acce2e4cf
“chiupam” <chiupam@126.com>
2021-07-21 14:14:14 +0800

    与测试版机器人同步文件

--- jbot/user.py
+++ jbot/user.py
@@ -1,95 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Author   : Chiupam
-# @Data     : 2021-07-13
-# @Version  : v 2.6
-# @Updata   :
-# @Future   :
-
-
-from .. import chat_id, jdbot, _ConfigDir, logger, api_id, api_hash, proxystart, proxy, _ScriptsDir, _OwnDir, _JdbotDir, TOKEN, _LogDir
-from ..bot.utils import cmd, press_event, backfile, jdcmd, _DiyDir, V4, QL, _ConfigFile, myck
+from .. import chat_id, jdbot, logger, api_id, api_hash, proxystart, proxy, _ConfigDir, _ScriptsDir, _JdbotDir
+from ..bot.utils import cmd, backfile, jdcmd, V4, QL, _ConfigFile, myck
+from ..diy.utils import getbean
 from telethon import events, TelegramClient
 import re, json, requests, asyncio, time, datetime, os
+
+from ..diy.utils import my_chat_id, bot_id, myzdjr_chatIds, myjoinTeam_chatIds
 
 
 if proxystart:
     client = TelegramClient("user", api_id, api_hash, proxy=proxy, connection_retries=None).start()
 else:
     client = TelegramClient("user", api_id, api_hash, connection_retries=None).start()
-
-
-with open(f"{_ConfigDir}/diybotset.json", 'r', encoding='utf-8') as f:
-    diybotset = json.load(f)
-my_chat_id = int(diybotset['my_chat_id'])
-
-
-bot_id = int(TOKEN.split(':')[0])
-
-
-def checkCookie1():
-    expired = []
-    cookies = myck(_ConfigFile)
-    for cookie in cookies:
-        cknum = cookies.index(cookie) + 1
-        if checkCookie2(cookie):
-            expired.append(cknum)
-    return expired, cookies
-
-
-def checkCookie2(cookie):
-    url = "https://me-api.jd.com/user_new/info/GetJDUserInfoUnion"
-    headers = {
-        "Host": "me-api.jd.com",
-        "Accept": "*/*",
-        "Connection": "keep-alive",
-        "Cookie": cookie,
-        "User-Agent": "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
-        "Accept-Language": "zh-cn",
-        "Referer": "https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&",
-        "Accept-Encoding": "gzip, deflate, br"
-    }
-    try:
-        r = requests.get(url, headers=headers).json()
-        if r['retcode'] == '1001':
-            return True
-        else:
-            return False
-    except:
-        return False
-
-
-def getbean(i, cookie, url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
-        "Accept-Encoding": "gzip,compress,br,deflate",
-        "Cookie": cookie,
-    }
-    result, o = '', '\n\t\t└'
-    try:
-        r = requests.get(url=url, headers=headers)
-        res = r.json()
-        if res['code'] == '0':
-            followDesc = res['result']['followDesc']
-            if followDesc.find('成功') != -1:
-                try:
-                    for n in range(len(res['result']['alreadyReceivedGifts'])):
-                        redWord = res['result']['alreadyReceivedGifts'][n]['redWord']
-                        rearWord = res['result']['alreadyReceivedGifts'][n]['rearWord']
-                        result += f"{o}领取成功，获得{redWord}{rearWord}"
-                except:
-                    giftsToast = res['result']['giftsToast'].split(' \n ')[1]
-                    result = f"{o}{giftsToast}"
-            elif followDesc.find('已经') != -1:
-                result = f"{o}{followDesc}"
-        else:
-            result = f"{o}Cookie 可能已经过期"
-    except Exception as e:
-        if str(e).find('(char 0)') != -1:
-            result = f"{o}访问发生错误：无法解析数据包"
-        else:
-            result = f"{o}访问发生错误：{e}"
-    return f"\n京东账号{i}{result}\n"
 
 
 # user?
@@ -142,13 +63,14 @@ async def zoo_shopbean(event):
         await jdbot.send_message(chat_id, info)
 
 
-@client.on(events.NewMessage(chats=[-1001112847619, -1001284907085, my_chat_id], pattern=r"export jd_zdjr_activityId=\".*\"|.*='.*'"))
+@client.on(events.NewMessage(chats=myzdjr_chatIds, pattern=r"export\sjd_zdjr_activityId=\".*\"|.*='.*'"))
 async def myzdjr(event):
     try:
-        cmdtext, end = False, False
+        msg = await jdbot.send_message(chat_id, '监控到 jd_zdjr_activityId 环境变量')
         messages = event.message.text.split("\n")
+        change = ''
         for message in messages:
-            kv = message.replace("export ", "").replace("*", "")
+            kv = message.replace("export ", "")
             kname = kv.split("=")[0]
             vname = re.findall(r"(\".*\"|'.*')", kv)[0][1:-1]
             with open(f"{_ConfigDir}/config.sh", 'r', encoding='utf-8') as f1:
@@ -157,7 +79,8 @@ async def myzdjr(event):
                 continue
             if configs.find(kname) != -1:
                 configs = re.sub(f'{kname}=(\"|\').*(\"|\')', kv, configs)
-                end = "替换环境变量成功"
+                change += f"替换 {kname} 环境变量成功\n{kv}\n\n"
+                msg = await jdbot.edit_message(msg, change)
             else:
                 if V4:
                     with open(f"{_ConfigDir}/config.sh", 'r', encoding='utf-8') as f2:
@@ -172,14 +95,65 @@ async def myzdjr(event):
                     with open(f"{_ConfigDir}/config.sh", 'r', encoding='utf-8') as f2:
                         configs = f2.read()
                     configs += f'export {kname}="{vname}"\n'
-                end = "新增环境变量成功"
+                change += f"新增 {kname} 环境变量成功\n{kv}\n\n"
+                msg = await jdbot.edit_message(msg, change)
             with open(f"{_ConfigDir}/config.sh", 'w', encoding='utf-8') as f3:
                 f3.write(configs)
-        if end:
-            await jdbot.send_message(chat_id, end)
+        if len(change) == 0:
+            await jdbot.edit_message(msg, "目前配置中的 jd_zdjr_activityId 环境变量无需改动")
+            return
         try:
             from ..diy.diy import smiek_jd_zdjr
             await smiek_jd_zdjr()
+        except:
+            None
+    except Exception as e:
+        await jdbot.send_message(chat_id, 'something wrong,I\'m sorry\n' + str(e))
+        logger.error('something wrong,I\'m sorry\n' + str(e))
+
+
+@client.on(events.NewMessage(chats=myjoinTeam_chatIds, pattern=r"^export\sjd_joinTeam_activityId=\".*\"|.*='.*'"))
+async def myjoinTeam(event):
+    try:
+        msg = await jdbot.send_message(chat_id, '监控到 jd_joinTeam_activityId 环境变量')
+        messages = event.message.text.split("\n")
+        change = ''
+        for message in messages:
+            kv = message.replace("export ", "")
+            kname = kv.split("=")[0]
+            vname = re.findall(r"(\".*\"|'.*')", kv)[0][1:-1]
+            with open(f"{_ConfigDir}/config.sh", 'r', encoding='utf-8') as f1:
+                configs = f1.read()
+            if vname in configs:
+                continue
+            if configs.find(kname) != -1:
+                configs = re.sub(f'{kname}=(\"|\').*(\"|\')', kv, configs)
+                change += f"替换 {kname} 环境变量成功\n{kv}\n\n"
+                msg = await jdbot.edit_message(msg, change)
+            else:
+                if V4:
+                    with open(f"{_ConfigDir}/config.sh", 'r', encoding='utf-8') as f2:
+                        configs = f2.readlines()
+                    for config in configs:
+                        if config.find("第五区域") != -1 and config.find("↑") != -1:
+                            end_line = configs.index(config)
+                            break
+                    configs.insert(end_line - 2, f'export {kname}="{vname}"\n')
+                    configs = ''.join(configs)
+                else:
+                    with open(f"{_ConfigDir}/config.sh", 'r', encoding='utf-8') as f2:
+                        configs = f2.read()
+                    configs += f'export {kname}="{vname}"\n'
+                change += f"新增 {kname} 环境变量成功\n{kv}\n\n"
+                msg = await jdbot.edit_message(msg, change)
+            with open(f"{_ConfigDir}/config.sh", 'w', encoding='utf-8') as f3:
+                f3.write(configs)
+        if len(change) == 0:
+            await jdbot.edit_message(msg, "目前配置中的 jd_joinTeam_activityId 环境变量无需改动")
+            return
+        try:
+            from ..diy.diy import jd_joinTeam_activityId
+            await jd_joinTeam_activityId()
         except:
             None
     except Exception as e:
