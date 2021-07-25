65fca196e371445a4fb302abb8f158efa2cb5ec8
“chiupam” <chiupam@126.com>
2021-07-25 20:32:15 +0800

    修复 cannot import name 'bot_id' from 'jbot.diy.utils' 的错误

--- jbot/checkcookie.py
+++ jbot/checkcookie.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
-from .. import chat_id, jdbot, logger, chname, mybot
+from .. import chat_id, jdbot, logger, chname, mybot, TOKEN
 from ..bot.utils import press_event, V4, QL, _ConfigFile, myck, _Auth
 from ..diy.utils import QL2, QL8, ql_token
 from telethon import events
 from asyncio import exceptions
 import requests, re, asyncio, time, sys, os
 
+bot_id = int(TOKEN.split(":")[0])
 
 async def checkCookie(cookie):
     url = "https://me-api.jd.com/user_new/info/GetJDUserInfoUnion"
