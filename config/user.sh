bd7579e843da93fd5be80a8111938b9c41c73703
“chiupam” <chiupam@126.com>
2021-08-11 21:47:05 +0800

    修复不存在 user.py 时无法安装 user.py 的憨批逻辑

--- config/user.sh
+++ config/user.sh
@@ -14,7 +14,7 @@ file_user=$dir_diy/user.py
 bot_set=$root/config/diybotset.json
 
 fix() {
-  if [ -z "$(grep -E "sdfasdf" $bot_set)" ]
+  if [ -z "$(grep -E "shoptokenId" $bot_set)" ]
     then echo "没有找到shoptokenId键，自动填写对应值！"
       key_1="shoptokenId"
       sed -i "s/key_1/$key_1/" $bot_set
