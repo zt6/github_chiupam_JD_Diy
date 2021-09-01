48af64f154ff326e3bc280572f4a00661d540723
“chiupam” <chiupam@126.com>
2021-09-01 18:48:43 +0800

    首次使用wskey功能时可以选择添加config.sh或者wskey.list中

--- beta/wskey.py
+++ beta/wskey.py
@@ -3,9 +3,9 @@
 
 
 from .. import chat_id, jdbot, logger, _ConfigDir
-from ..bot.utils import V4, _Auth
-from ..diy.utils import QL2, QL8, ql_token, wskey
-from telethon import events
+from ..bot.utils import V4, _Auth, press_event, split_list, row
+from ..diy.utils import QL2, QL8, ql_token, wskey, read, write
+from telethon import events, Button
 from requests import get, put, post
 import re
 import os
@@ -20,18 +20,57 @@ async def myaddwskey(event):
         messages = event.raw_text.split("\n")
         if V4:
             file = f"{_ConfigDir}/wskey.list"
-            if not os.path.exists(file):
-                os.system(f"touch {file}")
-            for message in messages:
-                pin = message.split(";")[0].split("=")[1]
-                configs = wskey("str")
-                if pin in configs:
-                    configs = re.sub(f"pin={pin};wskey=.*;", message, configs)
-                    text += f"更新wskey成功！pin为：{pin}\n"
-                else:
-                    configs += message + "\n"
-                    text += f"新增wskey成功！pin为：{pin}\n"
-                wskey(configs)
+            configs = read("str")
+            if not os.path.exists(file) and "wskey" not in configs:
+                sender = event.sender_id
+                async with jdbot.conversation(sender, timeout=120) as conv:
+                    tip = "由于这是你第一次使用此功能，关于wskey的存储位置，请做出您的选择："
+                    buttons = [
+                        Button.inline("存储在config.sh中", data="config.sh"),
+                        Button.inline("存储在wskey.list中", data="wskey.list"),
+                        Button.inline('取消会话', data='cancel')
+                    ]
+                    msg = await jdbot.edit_message(msg, tip, buttons=split_list(buttons, row))
+                    convdata = await conv.wait_event(press_event(sender))
+                    res = bytes.decode(convdata.data)
+                    if res == 'cancel':
+                        await jdbot.edit_message(msg, '对话已取消')
+                        return False
+                    elif res == 'wskey.list':
+                        os.system(f"touch {file}")
+                    msg = await jdbot.edit_message(msg, f'你的选择是：存储在{res}中\n准备继续工作……')
+            if os.path.exists(file):
+                for message in messages:
+                    pin = message.split(";")[0].split("=")[1]
+                    configs = wskey("str")
+                    if pin in configs:
+                        configs = re.sub(f"pin={pin};wskey=.*;", message, configs)
+                        text += f"更新wskey成功！pin为：{pin}\n"
+                    else:
+                        configs += message + "\n"
+                        text += f"新增wskey成功！pin为：{pin}\n"
+                    wskey(configs)
+            else:
+                for message in messages:
+                    pin = message.split(";")[0].split("=")[1]
+                    configs = read("str")
+                    if pin + ";wskey" in configs:
+                        configs = re.sub(f'pin={pin};wskey=.*;', message, configs)
+                        text += f"更新wskey成功！pin为：{pin}\n"
+                    else:
+                        configs = read("list")
+                        for config in configs:
+                            if pin in config:
+                                line = configs.index(config)
+                                num = re.findall(r'(?<=[Cc]ookie)[\d]+(?==")', config)[0]
+                                configs.insert(line, f'wskey{num}="{message}"\n')
+                                text += f"新增wskey成功！pin为：{pin}\n"
+                                break
+                            elif "第二区域" in config:
+                                await jdbot.edit_message(msg, "请使用标准模板！")
+                                return
+                    await jdbot.edit_message(msg, text)
+                    write(configs)
         elif QL8:
             token = ql_token(_Auth)
             for message in messages:
