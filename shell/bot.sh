606a3d204ac5d96c5f661dfa14f1b5931d67ab24
“chiupam” <chiupam@126.com>
2021-09-06 13:22:22 +0800

    使用原机器人的安装shell

--- shell/bot.sh
+++ shell/bot.sh
@@ -1,144 +1,112 @@
 #!/usr/bin/env bash
-
-if [ -d "/jd" ]; then
-  root=/jd
+# 从whyour 大佬的bot.sh 与E大的jup.sh 拼凑出来的
+## 导入通用变量与函数
+if [ ! -d "/ql" ];then
+  dir_root=/jd
 else
-  root=/ql
+  dir_root=/ql
 fi
+dir_bot=$dir_root/jbot
+dir_repo=$dir_root/repo
+file_bot_setting_user=$dir_root/config/bot.json
+repo_path="${dir_repo}/SuMaiKaDe_bot"
+url="https://ghproxy.com/https://github.com/SuMaiKaDe/bot.git"
+repo_path="${dir_repo}/dockerbot"
 
-diybot_url="https://github.com/chiupam/diybot.git"
-jbot_botset=$root/config/bot.json
-diybot_repo=$root/repo/diybot
-diybot_diy=$root/jbot/diy
-diybot_repo_diybotset=$root/repo/diybot/config/diybotset.json
-diybot_config_diybotset=$root/config/diybotset.json
-diybot_repo_user=$root/repo/diybot/jbot/diy/user.py
-diybot_diy_user=$root/jbot/diy/user.py
-diybot_repo_diy=$root/repo/diybot/jbot/diy/diy.py
-diybot_diy_diy=$root/jbot/diy/diy.py
-if [ $# = 1 ];then
-  up=$1
-else
-  up=no
-fi
-
-git_pull() {
-  local dir_current=$(pwd)
-  local url=$1
-  local dir=$2
-  local branch=$3
-  [[ $branch ]] && local cmd=origin/${branch}
-  cd $dir
-  echo "开始更新仓库 $url 到 $dir"
-  git fetch --all
-  git reset --hard $cmd
-  git pull
-  cd $dir_current
+git_pull_scripts() {
+    local dir_current=$(pwd)
+    local dir_work="$1"
+    local branch="$2"
+    [[ $branch ]] && local cmd="origin/${branch}"
+    cd $dir_work
+    echo -e "开始更新仓库：$dir_work\n"
+    git fetch --all
+    exit_status=$?
+    git reset --hard $cmd
+    git pull
+    cd $dir_current
 }
 
-git_clone() {
-  local url=$1
-  local dir=$2
-  local branch=$3
-  [[ $branch ]] && local cmd="-b $branch"
-  echo "开始克隆仓库 $url 到 $dir"
-  git clone $cmd $url $dir
+git_clone_scripts() {
+    local url=$1
+    local dir=$2
+    local branch=$3
+    [[ $branch ]] && local cmd="-b $branch "
+    echo -e "开始克隆仓库 $url 到 $dir\n"
+    git clone $cmd $url $dir
+    exit_status=$?
 }
+notify_telegram () {
+    local message="$(echo -e $1)"
+    local bot_token=$(cat $file_bot_setting_user | jq -r .bot_token)
+    local user_id=$(cat $file_bot_setting_user | jq .user_id)
+    local proxy=$(cat $file_bot_setting_user | jq .proxy)
+    local proxy_type=$(cat $file_bot_setting_user | jq -r .proxy_type)
+    local proxy_add=$(cat $file_bot_setting_user | jq -r .proxy_add)
+    local proxy_port=$(cat $file_bot_setting_user | jq .proxy_port)
+    local proxy_user=$(cat $file_bot_setting_user | jq -r .proxy_user)
+    local proxy_password=$(cat $file_bot_setting_user | jq -r .proxy_password)
+    local api_url="https://api.telegram.org/bot${bot_token}/sendMessage"
+    local cmd_proxy_user cmd_proxy
 
-diybot() {
-  if [ -d $diybot_repo ]; then
-    echo "更新 diybot 所需文件"
-    git_pull $diybot_url $diybot_repo "master"
-  else
-    echo "下载 diybot 所需文件"
-    git_clone $diybot_url $diybot_repo "master"
-  fi
-}
-
-fix() {
-  zoo_opencard=$(cat $diybot_config_diybotset | grep "zoo_opencard")
-  if [ -z "$zoo_opencard" ]
-    then echo "请先修改 $diybot_config_diybotset 的内容，再重新启动！参考链接如下"
-    echo ""
-    echo "https://raw.githubusercontent.com/chiupam/diybot/master/config/diybotset.json"
-    echo ""
-    exit
-  fi
-  shoptokenId=$(cat $diybot_config_diybotset | grep "shoptokenId")
-  if [ -z "$shoptokenId" ]
-    then echo "请先修改 $diybot_config_diybotset 的内容，再重新启动！参考链接如下"
-    echo ""
-    echo "https://raw.githubusercontent.com/chiupam/diybot/master/config/diybotset.json"
-    echo ""
-    exit
-  fi
-}
-
-file_diybotset() {
-  echo "检测 diybotset.json 文件 "
-  if [ -f $diybot_config_diybotset ]; then
-    echo "  └—结果：存在，不拉取"
-  else
-    echo "  └—结果：不存在，拉取"
-    cp $diybot_repo_diybotset $diybot_config_diybotset
-  fi
-}
-
-file_user() {
-  echo "检测 user.py 文件 "
-  if [ -f $diybot_diy_user ]; then
-    if [[ $up = user ]]; then
-      echo "  └—结果：存在，已选择更新"
-      cp -f $diybot_repo_user $diybot_diy_user
+    if [[ $proxy_user != *无则不用* ]] && [[ $proxy_password != *无则不用* ]]; then
+        cmd_proxy_user="--proxy-user $proxy_user:$proxy_password"
     else
-      echo "  └—结果：存在，默认不更新"
-      rm -f $diybot_repo_user
+        cmd_proxy_user=""
     fi
-  else
-    echo "  └—结果：不存在，删除"
-    rm -f $diybot_repo_user
-  fi
-}
 
-file_diy() {
-  echo "检测 diy.py 文件 "
-  if [ -f $diybot_diy_diy ]; then
-    echo "  └—结果：存在，不拉取"
-  else
-    echo "  └—结果：不存在，拉取"
-    cp -f $diybot_repo_diy $diybot_diy_diy
-  fi
-}
+    if [[ $proxy == true ]]; then
+        cmd_proxy="--proxy $proxy_type://$proxy_add:$proxy_port $cmd_proxy_user"
+    else
+        cmd_proxy=""
+    fi
 
-copy() {
-  echo "拉取diy机器人文件进入 $root/jbot/diy 目录"
-  cp -rf $root/repo/diybot/jbot/diy/* $root/jbot/diy
+    curl -Ss $cmd_proxy -H "Content-Type:application/x-www-form-urlencoded" -X POST -d "chat_id=${user_id}&text=${message}&disable_web_page_preview=true" "$api_url" &>/dev/null
 }
+echo -e "\n1、安装bot依赖...\n"
+apk --no-cache add -f zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
+echo -e "\nbot依赖安装成功...\n"
 
-start() {
-  if [ -z $(grep -E "123456789" $jbot_botset) ]
-    then if [ -d "/jd" ]
-      then cd $dir_jbot
-        pm2 start ecosystem.config.js
-        cd $root
-        pm2 restart jbot
-      else
+echo -e "2、下载bot所需文件...\n"
+if [ -d ${repo_path}/.git ]; then
+    jbot_md5sum_old=$(cd $dir_bot; find . -type f \( -name "*.py" -o -name "*.ttf" \) | xargs md5sum)
+    git_pull_scripts ${repo_path}
+    cp -rf "$repo_path/jbot" $dir_root
+    jbot_md5sum_new=$(cd $dir_bot; find . -type f \( -name "*.py" -o -name "*.ttf" \) | xargs md5sum)
+    if [[ "$jbot_md5sum_new" != "$jbot_md5sum_old" ]]; then
+        notify_telegram "检测到BOT程序有更新，BOT将重启。\n\n友情提醒：如果当前有从BOT端发起的正在运行的任务，将被中断。\n\n本条消息由jup程序通过BOT发出。"
+    fi
+else
+  git_clone_scripts ${url} ${repo_path} "main"
+  cp -rf "$repo_path/jbot" $dir_root
+fi
+if [[ ! -f "$dir_root/config/bot.json" ]]; then
+  cp -f "$repo_path/config/bot.json" "$dir_root/config"
+fi
+echo -e "\nbot文件下载成功...\n"
+echo -e "3、安装python3依赖...\n"
+cd $dir_bot
+pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+pip3 --default-timeout=100 install -r requirements.txt --no-cache-dir
+echo -e "\npython3依赖安装成功...\n"
+echo -e "4、启动bot程序...\n"
+cd $dir_root
+if [ ! -d "/ql/log/bot" ]; then
+    mkdir $dir_root/log/bot
+fi
+if [[ -z $(grep -E "123456789" $dir_root/config/bot.json) ]]; then
+    if [ -d "/ql" ]; then
         ps -ef | grep "python3 -m jbot" | grep -v grep | awk '{print $1}' | xargs kill -9 2>/dev/null
-        nohup python3 -m jbot >$root/log/bot/bot.log 2>&1 &
-      fi
-  else
-    echo "请修改${jbot_botset}的信息后再次手动启动"
-  fi
-}
-
-main() {
-  diybot
-  fix
-  file_diybotset
-  file_user
-  file_diy
-  copy
-  start
-}
-
-main
+        nohup python3 -m jbot >$dir_root/log/bot/bot.log 2>&1 &
+        echo -e "bot启动成功...\n"
+    else
+        cd $dir_bot
+        pm2 start ecosystem.config.js
+        cd $dir_root
+        pm2 restart jbot
+        echo -e "bot启动成功...\n"
+    fi
+else
+    echo -e  "似乎 $dir_root/config/bot.json 还未修改为你自己的信息，可能是首次部署容器，因此不启动Telegram Bot...\n配置好bot.json后再次运行本程序即可启动"
+fi
+exit 0
